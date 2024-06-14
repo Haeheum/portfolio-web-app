2a5b91342510a842f4f4a9e0762e553bf288a95d
haeheumjo <hammizzang@gmail.com>
2024-06-14 19:30:43 +0900

    fix: added shimmer effect

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "05b87a2470002e05d5c8bea227179faa",
+const RESOURCES = {"flutter_bootstrap.js": "65f0f4cee774f117f5331021e615ef48",
 "version.json": "0325640b78b9f97959290dc5ebb8b3ed",
 "index.html": "18a9d9c14d10a5ce4aabc124853237c4",
 "/": "18a9d9c14d10a5ce4aabc124853237c4",
-"main.dart.js": "358b5a3d4a4174343de46ab8386e9a51",
+"main.dart.js": "ee4cf72e5ecbe6f7bf2b499625c5f8fa",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
